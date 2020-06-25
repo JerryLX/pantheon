90a3679b4a5cec952337b6e0f10066a28dee1445
Jerry <jerry-lx@sjtu.edu.cn>
2020-06-24 23:36:00 -0400

    add autocc

---
+++ src/wrappers/autocc.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from os import path
+from subprocess import check_call
+
+import arg_parser
+import context
+
+
+def main():
+    args = arg_parser.sender_first()
+
+    cc_repo = path.join(context.third_party_dir, 'autocc')
+    send_src = path.join(cc_repo, 'run_sender.py')
+    recv_src = path.join(cc_repo, 'env', 'run_receiver.py')
+
+    if args.option == 'sender':
+        cmd = [send_src, args.port]
+        check_call(cmd)
+        return
+
+    if args.option == 'receiver':
+        cmd = [recv_src, args.ip, args.port]
+        check_call(cmd)
+        return
+
+
+if __name__ == '__main__':
+    main()
